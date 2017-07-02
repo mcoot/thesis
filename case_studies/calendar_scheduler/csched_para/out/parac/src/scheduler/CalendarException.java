c1e971694f14c997b4ecf06d42287532568754e7
JosephSpearritt <joseph.spearritt@uq.net.au>
2017-07-02 19:57:56 +1000

    asdfasdfasdf

---
+++ case_studies/calendar_scheduler/csched_para/out/parac/src/scheduler/CalendarException.java
@@ -0,0 +1,11 @@
+public class CalendarException extends java.lang.Exception
+{
+  public CalendarException ()
+  {
+    super();
+  }
+  public CalendarException (java.lang.String message)
+  {
+    super(message);
+  }
+}
