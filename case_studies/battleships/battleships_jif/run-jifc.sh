34c1a701d8b8bbc68fecf58b08c376af998ece6b
Joseph Spearritt <joe.spearritt@live.com>
2017-07-28 17:07:54 +1000

    Add single-class to jif ant build script

--- case_studies/battleships/battleships_jif/run-jifc.sh
+++ case_studies/battleships/battleships_jif/run-jifc.sh
@@ -1,2 +1,14 @@
-# echo "$JIFHOME/bin/jifc -d $2 -classpath $3:. $1/battleships/*.jif"
-$JIFHOME/bin/jifc -e -d "$2" -classpath "$3:." $1/battleships/*.jif
+#!/bin/bash
+
+verbString=""
+if [ "$5" == "true" ];
+then
+    verbString="-e"
+fi
+
+if [ "$4" == "all" ];
+then
+    $JIFHOME/bin/jifc $verbString -d "$2" -classpath "$3:." $1/battleships/*.jif
+else
+    $JIFHOME/bin/jifc $verbString -d "$2" -classpath "$3:." $1/battleships/$4.jif
+fi
