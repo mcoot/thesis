4254cb87efb307ac9a327ed74af90631b86a79cb
Joseph Spearritt <joe.spearritt@live.com>
2017-07-29 16:24:52 +1000

    asdfafasfa

--- case_studies/conference/conf_jif/run-jifc.sh
+++ case_studies/conference/conf_jif/run-jifc.sh
@@ -12,5 +12,5 @@ if [ "$4" == "all" ];
 then
     $JIFHOME/bin/jifc $verbString -d "$2" -sourcepath "$1" -classpath "$3" $1/conf/*.jif
 else
-    $JIFHOME/bin/jifc $verbString -d "$2" -sourcepath "$1" -classpath "$3" $1/conf/$4.jif
+    $JIFHOME/bin/jifc $verbString -d "$2" -sourcepath "$1" -classpath "$3" -nonrobust $1/conf/$4.jif
 fi
