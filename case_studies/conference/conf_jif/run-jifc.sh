876b4c152b164d7c070b278300f4bd02e419ea8a
Joseph Spearritt <joe.spearritt@live.com>
2017-07-30 14:43:22 +1000

    Fix run-jifc.sh wrong jifc compiler

--- case_studies/conference/conf_jif/run-jifc.sh
+++ case_studies/conference/conf_jif/run-jifc.sh
@@ -18,5 +18,5 @@ if [ "$4" == "all" ];
 then
     $JIFHOME/bin/jifc $verbString -d "$2" -sourcepath "$1" -classpath "$3" $1/conf/*.jif
 else
-    $JIFHOME/../jif-3.4.3/bin/jifc $verbString -d "$2" -sourcepath "$1" -classpath "$3" -nonrobust $1/conf/$4.jif
+    $JIFHOME/bin/jifc $verbString -d "$2" -sourcepath "$1" -classpath "$3" -nonrobust $1/conf/$4.jif
 fi
